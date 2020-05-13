90bbd5f9f289168dfdf964d690812848ecdeb826
hansin <huxinghan@loonxi.com>
2020-05-13 15:29:44 +0800

    fix diff

--- src/diff/index.js
+++ src/diff/index.js
@@ -1,41 +1,66 @@
-import React from '../react';
-import ReactDOM, { createElement, _render } from '../react-dom';
-import { diff } from './diff'
-import { patch } from './patch'
+import * as patchType from './patchType'
+let Index = 0;
 
-let VDom1 = createElement(
-    'ul',
-    { class: 'list' },
-    createElement('li', { class: 'item' }, [
-      createElement('a', { href: '#' }, 'link'),
-    ]),
-    createElement('li', { class: 'item', style: "color:red" }, 'b'),
-    createElement('li', { class: 'item' }, 'c'),
-)
+function diff(oldDOM, newDOM) {
+  let patchs = {}
+  let index = 0
+  // 遍历dom树
+  TreeWalker(oldDOM, newDOM, patchs, index)
+  return patchs;
+}
 
-let VDom2 = createElement(
-    'ul',
-    { class: 'lists', style: "color:red" }, 
-    createElement('li', { class: 'item' }, 'a'),
-    createElement('li', { class: 'item' }, 'b'),
-    createElement('li', { class: 'item' }, 'c'),
-)
+function TreeWalker(oldNode, newNode, patchs, index) {
+  let curPatch = []
+  // 节点被移除
+  if (!newNode) {
+    console.log('节点被移除 =>', oldNode)
+    curPatch.push({ type: patchType.REMOVE })
+  // 标签相同，则比较属性和子节点
+  } else if (oldNode.tag === newNode.tag) {
+    let props = diffProps(oldNode.props, newNode.props)
+    if (Object.keys(props).length > 0) {
+      curPatch.push({ type: patchType.ATTR, props })
+    }
+    //子节点为文本内容
+    if (isString(oldNode) && isString(newNode)) {
+      if (oldNode !== newNode) {
+        curPatch.push({ type: patchType.TEXT, text: newNode })
+      }
+    } else {
+      // 遍历子节点
+      oldNode.children.forEach((item, i) => {
+        TreeWalker(item, newNode.children[i], patchs, ++Index)
+      })
+    }
+  // 节点被替换
+  } else {
+    curPatch.push({ type: patchType.REPLACE, newNode })
+  }
 
-let VDom3 = createElement(
-    'ul',
-    { class: 'lists', 'data-key': 'aaa' },
-    createElement('li', { class: 'item', style: "color:black" }, 'aa'),
-    createElement('li', { class: 'item' }, '1'),
-    createElement('li', { class: 'item' }, 'bb')
-)
+  if (curPatch.length > 0) {
+    patchs[index] = curPatch
+  }
+}
 
-const el = _render(VDom2)
+function diffProps(oldProps, newProps) {
+  let patch = {}
+  //比较属性是否改变
+  for (let key in oldProps) {
+    if (oldProps[key] !== newProps[key]) {
+      patch[key] = newProps[key]
+    }
+  }
+  //比较属性是否新增
+  for (let key in newProps) {
+    if (!oldProps.hasOwnProperty(key)) {
+      patch[key] = newProps[key]
+    }
+  }
+  return patch;
+}
 
+function isString(node) {
+  return Object.prototype.toString.call(node) === '[object String]'
+}
 
-ReactDOM.render(VDom2, document.getElementById('root'))
-
-let patchs = diff(VDom2, VDom3);
-
-console.log('patchs=>', patchs)
-
-patch(el, patchs);
+export { diff }
